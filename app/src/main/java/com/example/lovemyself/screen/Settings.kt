5c94679dfed82e8171ac9f92ee67589d42b38bad
jeongmin <tmxk0810@gmail.com>
2024-05-08 17:20:25 +0900

    - 설정 시간 realtime database 에 저장 까지 구현 - 일부 string 추가
    2024. 5. 8(수)

--- app/src/main/java/com/example/lovemyself/screen/Settings.kt
+++ app/src/main/java/com/example/lovemyself/screen/Settings.kt
@@ -1,5 +1,7 @@
 package com.example.lovemyself.screen
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -13,8 +15,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
@@ -31,9 +36,11 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -68,7 +75,7 @@ fun SettingsScreen(backToMain: () -> Unit) {
                         })
                     SettingRow(text = stringResource(id = R.string.screen_lock), checked = lockValue,
                         turnOn = {
-                            settingViewModel.updateValue(false, lockValue.value)
+                            if(pinText.value.isNotEmpty()) settingViewModel.updateValue(false, lockValue.value)
                             navController.navigate(navRoute[2]) },
                         turnOff = {
                             settingViewModel.updateValue(false, lockValue.value)
@@ -82,7 +89,16 @@ fun SettingsScreen(backToMain: () -> Unit) {
                 }
             }
             composable(navRoute[1]) {
-                EnterAlarm(navController)
+                Column {
+                    EnterAlarm(alarmText, context, onClickBack = {
+                            settingViewModel.updateValue(isAlarm = true, isOn = false)
+                            navController.popBackStack()},
+                        updateTime = {
+                            settingViewModel.updateText(true, alarmText.value)
+                            settingViewModel.updateValue(isAlarm = true, isOn = true)
+                        }
+                    )
+                }
             }
             composable(navRoute[2]) {
                 pinText.value = ""
@@ -139,9 +155,95 @@ fun SettingRow(text: String, checked: MutableState<Boolean>, turnOn: () -> Unit,
 }
 
 @Composable
-fun EnterAlarm(navController: NavHostController) {
-    Column {
-        Appbar(stringResource(id = R.string.alarm)) { navController.popBackStack() }
+fun SelectTime(selectedTime: MutableState<String>, list: List<String>, modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .border(BorderStroke(1.dp, Color.LightGray))
+                .padding(horizontal = 20.dp, vertical = 10.dp)
+                .width(30.dp)
+                .height(48.dp)
+        ) {
+            list.forEach {
+                item {
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.bodyLarge.copy(if(selectedTime.value==it) Color.Black else Color.LightGray),
+                        modifier = Modifier.clickable { selectedTime.value = it }
+                    )
+                }
+            }
+        }
+        Text(text = selectedTime.value)
+    }
+}
+
+@Composable
+fun EnterAlarm(alarmText: MutableState<String>, context: Context, onClickBack: () -> Unit, updateTime: () -> Unit) {
+    val time = remember { mutableStateOf("") }
+    val timeList = listOf("AM", "PM")
+    val hour = remember { mutableStateOf("") }
+    val hourList = (1..12).toList().map { it.toString() }
+    val minute = remember { mutableStateOf("") }
+    val minuteList = (0..59).toList().map { it.toString() }
+    val confirm = remember { mutableStateOf(false) }
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(if (confirm.value) Color.Black.copy(0.12f) else Color.White)) {
+        Appbar(stringResource(id = R.string.alarm)) { onClickBack() }
+        Text(
+            text = stringResource(id = R.string.please_click),
+            style = MaterialTheme.typography.bodyLarge.copy(textAlign = TextAlign.Center, fontWeight = FontWeight.W800, color = BasicBlack),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            SelectTime(selectedTime = time, list = timeList, modifier = Modifier.weight(1f))
+            SelectTime(selectedTime = hour, list = hourList, modifier = Modifier.weight(1f))
+            SelectTime(selectedTime = minute, list = minuteList, modifier = Modifier.weight(1f))
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(id = R.string.setting),
+            style = MaterialTheme.typography.bodyMedium.copy(textAlign = TextAlign.Center, color = BasicBlack),
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { if(time.value.isNotEmpty() && hour.value.isNotEmpty() && minute.value.isNotEmpty()) confirm.value = true }
+        )
+    }
+
+    if(confirm.value) {
+        AlertDialog(
+            containerColor = Color.White,
+            onDismissRequest = { confirm.value = false },
+            confirmButton = {
+                Text(
+                    text = stringResource(id = R.string.yes),
+                    style = MaterialTheme.typography.labelMedium.copy(Color.Blue),
+                    modifier = Modifier.clickable {
+                        confirm.value = false
+                        alarmText.value = time.value + " " + hour.value + ":" + minute.value
+                        updateTime()
+                        Toast.makeText(context, context.getString(R.string.success_to_setting_alarm_time).format(time.value, hour.value, minute.value), Toast.LENGTH_SHORT).show()
+                    })
+                            },
+            dismissButton = {
+                Text(
+                    text = stringResource(id = R.string.no),
+                    style = MaterialTheme.typography.labelMedium.copy(Color.Red),
+                    modifier = Modifier.clickable { confirm.value = false }) },
+            text = {
+                Text(
+                    text = stringResource(id = R.string.confirm_alarm_time).format(time.value, hour.value, minute.value),
+                    style = MaterialTheme.typography.bodyLarge.copy(BasicBlack)
+                )
+            },
+            modifier = Modifier.padding(horizontal = 16.dp),
+            properties = DialogProperties(dismissOnClickOutside = false, usePlatformDefaultWidth = false)
+        )
     }
 }
 
